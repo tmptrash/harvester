20e9ee427442ba4eae589f9f99345300a989dcf1
flatline <deadbrainman@gmail.com>
2025-02-20 20:37:35 +0200

    tests refactoring

--- src/harvester.test.js
+++ src/harvester.test.js
@@ -1,9 +1,18 @@
 const { JSDOM } = require('jsdom')
 const harvest = require('./harvester')
 
+function testHarvester(tpl, html, query) {
+  const dom = new JSDOM(html)
+  return harvest(tpl, dom.window.document.querySelector(query))
+}
+
 describe('harvester library tests', () => {
+  let consoleSpy
+  beforeEach(() => consoleSpy = jest.spyOn(console, "error").mockImplementation(() => {}))
+  afterEach(() => consoleSpy.mockRestore())
+
   it('test if there is no pseudo template', () => {
-    const dom = new JSDOM(`
+    const ret = testHarvester('', `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -12,8 +21,7 @@ describe('harvester library tests', () => {
     <body>
     </body>
     </html>
-    `)
-    const ret = harvest('', dom.window.document.querySelector('body'))
+    `, 'body')
     expect(ret[0]).toEqual({})
     expect(ret[1]).toEqual(0)
     expect(ret[2]).toEqual(0)
@@ -24,35 +32,17 @@ describe('harvester library tests', () => {
     expect(ret[1]).toEqual(1)
     expect(ret[2]).toEqual(0)
   })
+  it('test if there is no DOM tree and pseudo template', () => {
+    const ret = harvest('', null)
+    expect(ret[0]).toEqual({})
+    expect(ret[1]).toEqual(0)
+    expect(ret[2]).toEqual(0)
+  })
   it('test for an empty template and wrong start padding', () => {
-    const dom = new JSDOM(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-    </head>
-    <body>
-      <div>
-        <span>
-          <h1>
-        </span>
-      </div>
-    </body>
-    </html>
-    `)
-    const tpl = `
+    const ret = testHarvester(`
      div
       span
-        h1`
-    const doc = dom.window.document
-    const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => {});
-    const [map] = harvest(tpl, doc.querySelector('body > div'))
-    expect(consoleSpy).toHaveBeenCalledWith("Error in line '     div' #:1. Wrong left indention. Must be a multiple of 2.")
-    expect(map).toEqual({})
-    consoleSpy.mockRestore();
-  })
-  it('test for an empty template and correct start padding', () => {
-    const dom = new JSDOM(`
+        h1`, `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -66,16 +56,30 @@ describe('harvester library tests', () => {
       </div>
     </body>
     </html>
-    `)
-    const tpl = `
+    `, 'body > div')
+    expect(consoleSpy).toHaveBeenCalledWith("Error in line '     div' #:1. Wrong left indention. Must be a multiple of 2.")
+    expect(ret[0]).toEqual({})
+  })
+  it('test for an empty template and correct start padding', () => {
+    const ret = testHarvester(`
     div
       span
-        h1`
-    const doc = dom.window.document
-    const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => {});
-    const [map] = harvest(tpl, doc.querySelector('body > div'))
+        h1`, `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+    </head>
+    <body>
+      <div>
+        <span>
+          <h1>
+        </span>
+      </div>
+    </body>
+    </html>
+    `, 'body > div')
     expect(consoleSpy).not.toHaveBeenCalled()
-    expect(map).toEqual({})
-    consoleSpy.mockRestore();
+    expect(ret[0]).toEqual({})
   })
 })

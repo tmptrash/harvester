6020cd8a2539b6c433ea4f295d7e573f74691e49
flatline <deadbrainman@gmail.com>
2025-04-28 16:08:32 +0300

    updated examples

--- examples/rozetka.js
+++ examples/rozetka.js
@@ -5,9 +5,9 @@ const PRODUCTS_QUERY = 'rz-product-tile'
 const TPL = `
   rz-product-tile
     div
-      a
+      *
         img[img=src]
-      a{title}
+      *{title}
       div
         rz-tile-price
           div{oldPrice}
@@ -17,6 +17,7 @@ const page = await open()
 
 await goto(page, async () => page.goto('https://rozetka.com.ua/'))
 const news = await page.evaluate((tpl, query) => {
+  window.price = function price (t, el) { return el.className.indexOf('price') > -1 }
   return Array.from(document.querySelectorAll(query)).map(el => harvest(tpl, el)[0])
 }, TPL, PRODUCTS_QUERY)
 

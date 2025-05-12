e578458ea3e4aa7b52af6baa6acd4a5ff857f1d4
flatline <deadbrainman@gmail.com>
2025-05-13 00:35:46 +0300

    * fixed an issue with amazon site for playwright * updated versions

--- examples/src/playwright/utils.js
+++ examples/src/playwright/utils.js
@@ -4,5 +4,6 @@ export const HARVESTER_PATH = './node_modules/js-harvester/src/harvester.js'
 
 export async function open () {
   const browser = await chromium.launch({ headless: false })
-  return browser.newPage()
+  const ctx = await browser.newContext({ userAgent: 'Chrome/111.0.0.0 Safari/537.36' })
+  return ctx.newPage()
 }
